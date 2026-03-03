81a3ec84bfb20071ff30ebf5276e7733df6a95ed
Luis Angel Muñoz Muñoz <luismm@lcg.unam.mx>
2026-03-02 22:08:22 -0600

    Version_final

--- nucleotide_count.py
+++ nucleotide_count.py
@@ -4,15 +4,17 @@ dna_sequence = " atg cga tta "
 # Limpieza
 dna_sequence = dna_sequence.strip()
 dna_sequence = dna_sequence.upper()
+dna_sequence_sinesp = dna_sequence.replace(" ","")
 
 # Conteos
-count_a = dna_sequence.count("A")
-count_t = dna_sequence.count("T")
-count_g = dna_sequence.count("G")
-count_c = dna_sequence.count("C")
+count_a = dna_sequence_sinesp.count("A")
+count_t = dna_sequence_sinesp.count("T")
+count_g = dna_sequence_sinesp.count("G")
+count_c = dna_sequence_sinesp.count("C")
 
 # Longitud
 length = count_a + count_t + count_c + count_g
 
 # Output
-print(f"Length:", {length})
+print(f"La longitud de la secuencia es de:", {length})
+print(dna_sequence_sinesp)
